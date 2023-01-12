ec8d8acdba52d846b3ce69d54240616866c3f5bf
sergey96 <altronxr@gmail.com>
2023-01-12 11:16:06 +0200

    create new project

---
+++ Main.php
@@ -0,0 +1,287 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Style.css">
+    <link rel="stylesheet" href="Adaptive.css">
+    <title>Auto-shop</title>
+</head>
+<body>
+
+<div class="Main flex_mod">
+<img id="btn_up" src="media/strUP_1.png" width="45" height="45" alt="">
+
+
+<div class="Block_1">
+<img id="logo" src="media/logoB.svg" width="12%" alt="">
+<img id="btn_menu" src="media/menu.png" width="30" height="30" alt="">
+<div class="nav_fon"></div>
+
+<aside>
+<div>
+  <img id="logo_nav" src="media/logoB.svg" width="12%" alt="">
+  <img id="btn_close" src="media/wClose.png" width="30" height="30" alt="">
+</div>
+
+  <ul>
+   <a href="#"><li>Скидки</li></a>
+   <a href="#"><li>Цены</li></a> 
+   <a href="#"><li>Услуги</li></a>
+   <a href="#"><li>Партнерам</li></a> 
+   <a href="#"><li>Про нас</li></a> 
+  </ul>
+
+<div class="nav_contacts">
+  <div>
+  +38095544656 - Менеджер<br/>
+  04455685566 - гл. Офис<br/>
+  г.Киев, ул.Булаховского 30а, кв.113
+  </div>
+
+  <nav>
+    <img src="media/card2.png" alt="" />
+    <img src="media/card3.png" alt="" />
+    <img src="media/card4.png" alt="" />
+</nav>
+</div>
+</aside>
+
+<header class="menu_list">
+  <a href="#"> <p name="promo">Скидка -30%</p> </a>
+  <a href="#"> <p>Цены</p> </a>
+  <a href="#"> <p>Партнерам</p> </a>
+  <p name="about_comp">Про нас</p>
+</header>
+
+<details class="bl_contacts">
+    <summary>Контакты&#9207;</summary>
+<div class="bl_contacts_menu">
+<p>
++38095544656 - Менеджер<br/>
+04455685566 - гл. Офис<br/>
+г.Киев, ул.Булаховского 30а, кв.113
+</p>
+  <img src="media/card2.png" alt="" />
+  <img src="media/card3.png" alt="" />
+  <img src="media/card4.png" alt="" />
+</div>
+</details>
+
+
+<div class="bl_bigFoto_1"></div>
+<div class="bl_bigFoto_2"></div>
+<div class="bl_bigFoto_3"></div>
+
+<div class="bl_text">
+<b>Наши услуги:</b> <br/>
+<ul>
+  <li>&#10004; Демонтаж зданий</li>
+  <li>&#10004; Аренда спецтехники</li>
+  <li>&#10004; Вывоз строймусора</li>
+  <li>&#10004; Разборка котлованов</li>
+</ul><br/>
+<button id="btn_service">Выбрать услугу</button>
+</div>
+
+<div class="bl_callBack">
+ <span id="timer"> Оставьте номер тел. и мы перезвоним вам через 1 мин.</span> <br/> <br/>
+  <input type="text" placeholder="+380"> <button id="btn_call">Жду звонка</button>
+</div>
+</div>
+
+
+
+
+<div class="Block_2 flex_mod">
+
+<section class="bl_Servise">
+  <div class="sv serv_1"> <img src="media/serv1.jpg" alt=""> <p> Аренда спецтехники </p> </div>
+  <div class="sv serv_2"> <img src="media/serv3.jpg" alt="">  <p> Выкопка котлованов </p> </div>
+  <div class="sv serv_3"> <img src="media/foto_3.jpg" alt="">  <p> Демонтаж зданий </p> </div>
+  <div class="sv serv_4"> <img src="media/serv2.jpg" alt="">  <p> Вывоз строймусора</p> </div>
+</section>
+
+<section class="bl_about">
+  <div class="foto_profi">
+  <img id="profi" src="media/profi.jpg" alt="">
+  <p name="text_profi">Непрерывно развиваясь и создавая свою репутацию, мы с 2009 года 
+    предоставляем профильные услуги частным лицам и коммерческим организациям.
+  </p>
+  </div>
+</section>
+
+<img id="kran" src="media/kran.png"  alt="">
+
+<div class="bl_why">
+  <p>Почему мы лучшие ?</p><br/><br/>
+- Экскаваторы разного размера, которые можно настроить в соответствии с вашими задачами;<br/><br/>
+- Ежедневные, еженедельные, ежемесячные и долгосрочные соглашения для выполнения Ваших потребностей;<br/><br/>
+- Оборудование, которое тщательно обслуживается и тестируется нашими квалифицированными специалистами;<br/><br/>
+- Доступная экстренная доставка;<br/><br/>
+- Внимательное обслуживание клиентов, и индивидуальный подход, чтобы обеспечить Ваши потребности;<br/><br/>
+- Низкие цены на аренду гусеничные и колесных экскаваторов;<br/><br/>
+- Быстрое выполнение взятых на себя обязательств. <br/>
+</div>
+</div>
+
+
+
+
+<div class="Block_3">
+
+<section class="block_partners flex_mod">
+ <span>Наши партнеры</span>
+
+ <div class="logo_line_partners">
+ <img src="media/lg_kl_1.svg" alt="">
+<img src="media/lg_kl_2.jpg" alt="">
+<img id="f_p_3" src="media/lg_kl_3.png" width="85" height="60" alt="">
+<img src="media/lg_kl_4.jpg" alt="">
+<img src="media/lg_kl_5.jpg" alt="">
+<img src="media/lg_kl_6.png" alt="">
+
+
+<img src="media/lg_kl_1.svg" alt="">
+<img src="media/lg_kl_2.jpg" alt="">
+<img id="f_p_3" src="media/lg_kl_3.png" width="85" height="60" alt="">
+<img src="media/lg_kl_4.jpg" alt="">
+<img src="media/lg_kl_5.jpg" alt="">
+<img src="media/lg_kl_6.png" alt="">
+</div>
+
+</section>
+
+
+<section class="bl_otziv">
+<span>Отзывы наших клиентов</span>
+<div class="bl_klients">
+  <div>
+  <p><img src="media/tech2.jpg"  alt=""><br/><b>Ирина, г. Черкассы</b></p><br/>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque consequatur veritatis dolorum 
+    enim unde vel quia corporis error alias, molestias ullam voluptates ea, adipisci perferendis 
+    iure blanditiis, nihil explicabo laudantium.
+    enim unde vel quia corporis error alias, molestias ullam voluptates ea. 
+</p>
+</div>
+  <div><p><img src="media/tech3.jpg"  alt=""><br/><b>Владимир, г. Суммы</b></p><br/>
+  <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque consequatur veritatis dolorum 
+    enim unde vel quia corporis error alias, molestias ullam voluptates ea, adipisci perferendis 
+    iure blanditiis, nihil explicabo laudantium.</p></div>
+
+
+  <div><p><img src="media/tech2.jpg"  alt=""><br/><b>Катя, г. Одесса</b></p><br/>
+  <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque consequatur veritatis dolorum 
+    enim unde vel quia corporis error alias, molestias ullam voluptates ea, adipisci perferendis 
+    enim unde vel quia corporis error alias, molestias ullam voluptates ea, adipisci perferendis 
+    iure blanditiis, nihil </p></div>
+
+
+  <div><p><img src="media/tech3.jpg"  alt=""><br/><b>Игорь, г. Черкассы</b></p><br/>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque consequatur veritatis dolorum 
+    iure blanditiis, nihil explicabo laudantium.</p></div>
+</div>
+<button id="btn_feedback">Смотреть отзывы</button>
+</section>
+
+</div>
+
+
+
+
+
+<div class="Block_4 flex_mod">
+<h1 class="h_1">Немного о нас</h1>
+
+<div class="wripe_about_1">
+<img id="logoB2" src="media/logoB.svg" width="20%" alt="">
+<p>Компания "SCC" была основана в 2007 году и сейчас работает по всей Украине. <br/>
+Мы внимательно изучаем потребности рынка, внедряем прогрессивные строительные технологии, 
+используем современные материалы, технические средства и инструменты. 
+Наши специалисты имеют надлежащую квалификацию и весомый практический опыт проектирования и 
+строительства объектов разного уровня сложности. 
+</p>
+</div>
+
+<section class="wripe_about_2">
+<div class="bl_team">
+<img id="team" src="media/team.jpeg"  alt="">
+<p>У нас работают только лучшие сотрудники, которые в любой момент предоставят <br/>
+  вам профиссиональную помощь и консультацию</p></div>
+
+ <div>
+<img id="chief" src="media/our_chief.jpg" alt="">
+<p>Меня зовут Иванов Ивонович, обращайтесь<br/> 
+ ко мне по всем вопросам проводимых нами работ<br/> 
+  <b>	&#9993; Fdshj@.gmail.com</b>
+</p>
+</div>
+</section>
+
+<div class="wripe_about_3">
+<img id="sertf_1" src="media/sertif_1.jpg" alt="">
+<div class="wrap_pdf"><img id="foto_pdf" src="media/filePdf.png" width="130" height="130" alt=""> <br/><br/><br/><br/> <p>Сертификат качества</p> </div>
+<img  id="sertf_2" src="media/sertif_1.png" alt="">
+</div>
+
+<img id="doroga" src="media/doroga.png" width="100%" height="350" alt="">
+
+</div>
+
+</div>
+
+
+
+
+
+
+<footer class="Block_foter">
+<section class="sec_footer">
+<div class="foter_menu f_p">
+   <p>Меню</p>
+  <ul>
+    <li>Каталог</li>
+    <li>Клиентам</li>
+    <li>Карьера</li>
+    <li>Сервис центры</li>
+  </ul>
+</div>
+
+
+<div class="partners f_p">
+  <p>Наши партнеры:</p>
+<div class="our_partners ">
+  <img src="media/lg_kl_1.svg" alt="">
+  <img src="media/lg_kl_2.jpg" alt="">
+  <img src="media/lg_kl_3.png" alt="">
+  <img src="media/lg_kl_4.jpg" alt="">
+  <img src="media/lg_kl_5.jpg" alt="">
+</div>
+</div>
+
+<div class="our_contacts f_p">
+<p>Связаться с нами:</p>
+  +38050667844545 - Менеджер
+  +38050667844545 - гл. Офис
+  +38050667844545 - Директор
+</div>
+</section>
+
+
+<div class="foter_info">
+<img id="logo_footer" src="media/logoB.svg" alt="">
+<p>Все права защищены. 2023г.</p>
+</div>
+
+</footer>
+
+
+
+</div><!----- END Main ------->
+
+<!-- <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script> -->
+<script src="bild.js"></script>
+</body>
+</html>
